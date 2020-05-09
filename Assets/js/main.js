ab0832d4618e7427202df0fd819a811e2f1ad3ce
ryand67 <rwdevenney@gmail.com>
2020-05-09 00:11:24 -0400

    finished very quickly, didn't think to commit through the process so this is likely final commit except for readme or some slight optimizations.

---
+++ Assets/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    //Grabs the date based on the number returned from the getDay/etc methods
+    var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    //Creates new Date object and formats the top of the page 
+    var dateVar = new Date();
+    $("#currentDay").text(weekdays[dateVar.getDay()] + ", " + month[dateVar.getMonth()] + " " + dateVar.getDate());
+    //For every block, set the value to the local storage for the associated block
+    $(".description").each(function() {
+        $(this).val(localStorage.getItem("event" + $(this).attr("data-hour")));
+    })
+    //Timer compares current hour vs the associated block hour and assigns the past present/future class appropriately.  Removes any other classes that may have been assigned at another hour.  Updates every second.
+    function startTimer() {
+        var interval = setInterval(function() {
+                $(".description").each(function(){
+                    var currentHour = parseInt($(this).attr("data-hour"));
+                    if(currentHour < dateVar.getHours()) {
+                        $(this).removeClass("future");
+                        $(this).removeClass("present");
+                        $(this).addClass("past");
+                    } else if(currentHour > dateVar.getHours()) {
+                        $(this).removeClass("present");
+                        $(this).removeClass("past");
+                        $(this).addClass("future");
+                    } else if(currentHour === dateVar.getHours()) {
+                        $(this).removeClass("future");
+                        $(this).removeClass("past");
+                        $(this).addClass("present");
+                    }
+                })
+        }, 1000)
+    }
+    //Assigns on click to every button, saves the textarea text to the conveinince variable calEvent and then sets the associated local storage space
+    $(".btn").on("click", function() {
+        var calEvent = $(this).siblings(".description").val(); 
+        localStorage.setItem("event" + $(this).siblings(".description").attr("data-hour"), calEvent);
+    })  
+    //Stars the timer to keep the calendar colors updated
+    startTimer();
+})
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Work-Day-Schedule
+
+In this project I used jQuery, Date objects and data attributes to program a daily planner app.  The hourly events get saved and pulled from localStorage, and the hourly block's color updates based on whether the hour is in the past, present, or future.
